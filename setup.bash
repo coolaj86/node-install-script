6f4a429a2536546d25dd040917f52bdc232779f4
mathwhiz1212 <mathwhiz1212@users.noreply.github.com>
2016-05-11 15:23:51 -0600

    Added Trisquel support

--- setup.bash
+++ setup.bash
@@ -98,6 +98,8 @@ elif [ "$(uname | grep -i 'Linux')" ]; then
     OS='ubuntu'
   elif [ "$(cat /etc/issue | grep -i 'Debian')" ]; then
     OS='ubuntu'
+  elif [ "$(cat /etc/issue | grep -i 'Trisquel')" ]; then
+    OS='ubuntu'
   elif [ "$(cat /etc/issue | grep -i 'Raspbian')" ]; then
     OS='raspbian'
   elif [ "$(cat /etc/issue | grep -i 'Fedora')" ]; then
